08b49b89daf711d5ece07c2b1bad26dff67730d8
fanz <251327341@qq.com>
2014-11-04 15:45:54 +0800

    update dir helper

--- application/helpers/dir_helper.php
+++ application/helpers/dir_helper.php
@@ -9,11 +9,11 @@
 if ( ! function_exists('new_dir'))
 {
 	function new_dir($full_path) {
-		log_message('debug', "mkdir in ".$full_path)
+		log_message('debug', "mkdir in ".$full_path);
  		if(is_dir($full_path) == TRUE)
  			return TRUE;
  		else {
- 			if(mkdir($full_path))
+ 			if(mkdir($full_path, 0777))
  				return TRUE;
  			else
  				return FALSE;
